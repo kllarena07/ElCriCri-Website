be9380b8403151edaec48c46e86466ac056708a1
Kieran Llarena <baymax709@gmail.com>
2022-12-24 18:04:04 -0500

    created app template

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,29 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <section id="slideshow-container" class="section-container">
+    <span>this is the image side</span>
+  </section>
+  <section id="info-container" class="section-container">
+    <span>this is the info side</span>
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  @import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
+  .section-container {
+    color: white;
+    font-family: 'Source Code Pro', monospace;
+    background-color: black;
+    width: 50vw;
+    height: 100vh;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  #slideshow-container {
+    float: left;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  #info-container {
+    float: right;
   }
 </style>
